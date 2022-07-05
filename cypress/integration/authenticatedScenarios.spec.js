a892214890ba041d5478038735166332847d3175
= <=>
2022-07-04 21:13:20 -0300

    Create e2e tests with cypress

---
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -0,0 +1,46 @@
+import faker from 'faker'
+
+describe('Scenarios where a authenticaded user is required', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.login()
+  })
+  it('CRUDs notes', () => {
+    const noteDescription = faker.lorem.words(4)
+    cy.addNewNote(noteDescription)
+    cy.wait('@getNotes')
+
+    const updatedNoteDescription = faker.lorem.words(4)
+    cy.updateNote(noteDescription, updatedNoteDescription, true)
+    cy.wait('@getNotes')
+
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait('@getNote')
+    cy.wait('@getNotes')
+  })
+
+  it('Successfully subit credit card form', () => {
+    cy.intercept('POST', '**/prod/billing').as('paymentRequest')
+    cy.fillSettingsFormAndSubmit()
+
+    cy.wait('@getNotes')
+    cy.wait('@paymentRequest').then(response => {
+      expect(response.state).to.equal('Complete')
+    })
+  })
+
+  it.only('logs out', () => {
+    cy.visit('/')
+    cy.wait('@getNotes')
+    if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
+      cy.get('.navbar-toggle.collapsed')
+        .should('be.visible')
+        .click()
+    }
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.nav > :nth-child(2) > a').click()
+    cy.get('#email').should('be.visible')
+    cy.get('#password').should('be.visible')
+    /* ==== End Cypress Studio ==== */
+  })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,88 @@
+Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
+  cy.visit('/signup')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
+})
+Cypress.Commands.add(
+  'login',
+  (
+    username = Cypress.env('USER_EMAIL'),
+    password = Cypress.env('USER_PASSWORD'),
+    { cacheSession = true } = {}
+  ) => {
+    const login = () => {
+      cy.visit('/login')
+      cy.get('#email').type(username)
+      cy.get('#password').type(password, { log: false })
+      cy.contains('button', 'Login').click()
+      cy.contains('h1', 'Your Notes').should('be.visible')
+    }
+
+    if (cacheSession) {
+      cy.session([username, password], login)
+    } else {
+      login()
+    }
+  }
+)
+
+Cypress.Commands.add('addNewNote', (noteDescription, attachFile = false) => {
+  cy.visit('/notes/new')
+  cy.get('#content').type(noteDescription)
+
+  if (attachFile) {
+    cy.get('#file').attachFile('example.json')
+  }
+
+  cy.contains('button', 'Create').click()
+  cy.contains('.list-group-item', noteDescription).should('be.visible')
+})
+
+Cypress.Commands.add(
+  'updateNote',
+  (currentNoteDescription, newNoteDescription, attachFile = false) => {
+    cy.intercept('GET', '**/notes/**').as('getNote')
+
+    cy.contains('.list-group-item', currentNoteDescription).click()
+    cy.wait('@getNote')
+
+    cy.get('#content').clear().type(newNoteDescription)
+
+    if (attachFile) {
+      cy.get('#file').attachFile('example.json')
+    }
+
+    cy.contains('button', 'Save').click()
+    cy.contains('.list-group-item', currentNoteDescription).should('not.exist')
+    cy.contains('.list-group-item', newNoteDescription).should('be.visible')
+  }
+)
+
+Cypress.Commands.add('deleteNote', (noteDescription) => {
+  cy.intercept('GET', '**/notes/**').as('getNote')
+
+  cy.contains('.list-group-item', noteDescription).click()
+  cy.wait('@getNote')
+
+  cy.contains('button', 'Delete').click()
+  cy.contains('.list-group-item', noteDescription).should('not.exist')
+})
+
+Cypress.Commands.add('fillSettingsFormAndSubmit', () => {
+  cy.visit('/settings')
+  cy.get('#storage').type('1')
+  cy.get('#name').type('Mary Doe')
+
+  cy.iframe('.card-field iframe')
+    .as('iframe')
+    .find('[name="cardnumber"]')
+    .type('4242424242424242')
+
+  cy.get('@iframe').find('[name="exp-date"]').type('1271')
+  cy.get('@iframe').find('[name="cvc"]').type('123')
+  cy.get('@iframe').find('[name="postal"]').type('12345')
+  cy.contains('button', 'Purchase').click()
+})
