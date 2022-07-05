a79382d3a5a720b27694e00f8a7e7b10d604efa3
JVGC <joaovictorgarciacoelho@gmail.com>
2022-07-04 21:26:42 -0300

    Deprecating SignUp spec due to mailosaur trial no longer is available

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,21 +1,23 @@
-import faker from 'faker'
+/* THIS TEST NO LONGER WORKS DUE TO MAILOSAUR TRIAL ACCOUNT NO LONGER IS AVAILABLE */
 
-it('successfully signs up using confirmation code sent via email', () => {
-  const emailAddress = `${faker.datatype.uuid()}@${Cypress.env(
-    'MAILOSAUR_SERVER_ID'
-  )}.mailosaur.net`
-  const password = Cypress.env('USER_PASSWORD')
+// import faker from 'faker'
 
-  cy.intercept('GET', '**/notes').as('getNotes')
-  cy.fillSignupFormAndSubmit(emailAddress, password)
+// it('successfully signs up using confirmation code sent via email', () => {
+//   const emailAddress = `${faker.datatype.uuid()}@${Cypress.env(
+//     'MAILOSAUR_SERVER_ID'
+//   )}.mailosaur.net`
+//   const password = Cypress.env('USER_PASSWORD')
 
-  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-    sentTo: emailAddress,
-  }).then((message) => {
-    const confirmationCode = message.html.body.match(/\d{6}/)[0]
-    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+//   cy.intercept('GET', '**/notes').as('getNotes')
+//   cy.fillSignupFormAndSubmit(emailAddress, password)
 
-    cy.wait('@getNotes')
-    cy.contains('h1', 'Your Notes').should('be.visible')
-  })
-})
+//   cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+//     sentTo: emailAddress,
+//   }).then((message) => {
+//     const confirmationCode = message.html.body.match(/\d{6}/)[0]
+//     cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+
+//     cy.wait('@getNotes')
+//     cy.contains('h1', 'Your Notes').should('be.visible')
+//   })
+// })
